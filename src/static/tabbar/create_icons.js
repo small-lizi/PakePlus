13b987a2983a4719a16983ced427ca6a98156af8
small-lizi <162111696+small-lizi@users.noreply.github.com>
2025-06-13 16:39:07 +0800

    update src file from pakeplus

---
+++ src/static/tabbar/create_icons.js
@@ -0,0 +1,16 @@
+/**
+ * 请在此目录下添加以下图标文件:
+ * 
+ * 1. room.png - 房间管理未选中图标
+ * 2. room-active.png - 房间管理选中图标
+ * 3. user.png - 用户管理未选中图标
+ * 4. user-active.png - 用户管理选中图标
+ * 5. profile.png - 我的未选中图标
+ * 6. profile-active.png - 我的选中图标
+ * 
+ * 图标建议尺寸: 64x64px
+ * 
+ * 注意: 由于uniapp对tabBar图标有严格要求，图标必须存放在static目录下
+ */
+
+console.log('请添加必要的tabbar图标文件'); 
